84f757d34dd749108d15ed36262f709fb9c84d7d
Your Name <vemasani.pavani@gmail.com>
2025-08-25 09:08:21 +0530

    helm NGINX ingress

--- ingress-helm/ingress.sh
+++ ingress-helm/ingress.sh
@@ -1,3 +1,4 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 #helm show values ingress-nginx/ingress-nginx > custom-values.yaml
 helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx -f ingress.yaml
+ echo ${kubectl get svc nginx-ingress-ingress-nginx-controller | awk '{print $4}' | tail -1}
