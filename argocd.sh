b5508c7828d89fcfda9e4072cb27c375f4c999db
Your Name <vemasani.pavani@gmail.com>
2025-08-26 01:10:30 +0530

    helm NGINX ingress

--- argocd.sh
+++ argocd.sh
@@ -1,5 +1,3 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
   argocd login $(kubectl get ingress -A| grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
    for app in frontend backend; do
    argocd app create ${app} --repo https://github.com/pdevops78/expense-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default --insecure  --grpc-web --values values/${app}.yaml
