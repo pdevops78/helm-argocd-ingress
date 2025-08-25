afc42c2f2ff8d2a218158cda1d232cd84314b19d
Your Name <vemasani.pavani@gmail.com>
2025-08-26 00:06:29 +0530

    helm NGINX ingress

--- ingress-helm/ingress.sh
+++ ingress-helm/ingress.sh
@@ -1,7 +1,5 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-#helm show values ingress-nginx/ingress-nginx > custom-values.yaml
 helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx -f ingress.yaml
-# echo ${kubectl get svc nginx-ingress-ingress-nginx-controller | awk '{print $4}' | tail -1}
 kubectl create namespace argocd
 kubectl apply -f ingress-dev.yaml -n argocd
 
