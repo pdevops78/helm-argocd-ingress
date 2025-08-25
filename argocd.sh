49e8f21eefa849b801a291e70a99c11f5ea56f8b
Your Name <vemasani.pavani@gmail.com>
2025-08-26 00:47:06 +0530

    helm NGINX ingress

--- argocd.sh
+++ argocd.sh
@@ -1,10 +1,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-echo ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1}
-echo ${argocd admin initial-password -n argocd}
-
-#kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
-sleep 2
-
-argocd app create backend --repo https://github.com/pdevops78/learn-kubernetes.git --path expense-chart/chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure --skip-test-tls --grpc-web --values expense-chart/values/backend.yaml
+ argocd login ${kubectl get ingress -A| grep argocd | awk '{print $4}' | tail -1} --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+   for app in frontend backend; do
+   argocd app create ${app} --repo https://github.com/pdevops78/expense-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default --insecure  --grpc-web --values values/${app}.yaml
+   done
