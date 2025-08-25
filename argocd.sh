4b4cc77b794a372d21efba7249c76e7ddf05b66c
Your Name <vemasani.pavani@gmail.com>
2025-08-25 09:41:47 +0530

    helm NGINX ingress

--- argocd.sh
+++ argocd.sh
@@ -1,6 +1,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 echo ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1}
 echo ${argocd admin initial-password -n argocd}
 
