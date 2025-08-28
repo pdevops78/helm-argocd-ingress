8a57698fa7ec8484f8bd9caa2034aa92b06092da
Your Name <vemasani.pavani@gmail.com>
2025-08-28 16:02:28 +0530

    repo name changed

--- argocd.sh
+++
@@ -1,6 +0,0 @@
-  argocd login $(kubectl get ingress -A| grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure
-#  argocd login $(kubectl get ingress -A| grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
-   for app in frontend backend; do
-   argocd app create ${app} --repo https://github.com/pdevops78/helm-argocd-ingress.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default --insecure  --grpc-web --values values/${app}.yaml
-   done
-
