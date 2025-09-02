c1d0f48fecda7f500d57cfc71b57a8c63a73984c
Your Name <vemasani.pavani@gmail.com>
2025-09-02 16:08:37 +0530

    eks

--- ingress-helm-chart/install.sh
+++ ingress-helm-chart/install.sh
@@ -1,6 +1,6 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 # service for nlb
-helm upgrade --install ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml  --set controller.enableSslPassthrough=true
+helm upgrade --install ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 kubectl create namespace  argocd
 kubectl apply -f argocd-ingress-dev.yaml -n argocd
 
